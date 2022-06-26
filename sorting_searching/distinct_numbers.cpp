98a41d5035e241f88e413f2389449cdb1571a163
Salil Kumar <saliltrivedi47@gmail.com>
2022-06-26 21:25:37 +0530

    1621

---
+++ sorting_searching/distinct_numbers.cpp
@@ -0,0 +1,65 @@
+/*
+	Author° : Sangha
+	Description : https://cses.fi/problemset/task/1621
+	Date : 26-06-22 21:22
+*/
+
+#include<bits/stdc++.h>
+
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("O3")
+#pragma GCC optimize("unroll-loops")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+
+typedef long long ll;
+typedef long long int lli;
+//#define int long long int  // If u uncomment this , comment upper one (long long int thing)
+
+//n!=0 and (n-1)&(n) To check for power of 2
+
+#define INF (1<<29)
+#define LINF (1ll<<60)
+#define deb(x) cout<<#x<<" = "<<x<<"\n"
+#define all(x) (x).begin(),(x).end()
+
+const int mod = 1e9+7;
+
+using namespace std;
+
+void desperate_optimization(bool submitting=true,int precision=10)
+{
+  if(!submitting) return;
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+  cout.tie(0);
+  cout.setf(ios::fixed);
+  cout.setf(ios::showpoint);
+  cout.precision(precision);
+}
+
+int main()
+{
+    desperate_optimization(false);
+	int n,m,q,node,edge,x,y,i,j,count=0,temp,flag1=0,flag2=0;
+
+	cin >> n;
+	
+	// Set related problem
+	
+	set<int> S;
+	
+	while(n--)
+	{
+		cin >> temp ;
+		S.insert(temp);
+	}
+
+	cout << S.size() ;
+
+	int do_not_hack;
+
+	return 0;
+}
+
+
+
